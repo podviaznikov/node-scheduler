f2d4765185f9d49c0e13d6a2ece7031bd06dce55
Anton Podviaznikov <podviaznikov@gmail.com>
2010-12-05 13:37:10 +0100

    removed obsolete files

--- test.js
+++
@@ -1,6 +0,0 @@
-var cron = require('./cron'),
-sys = require('sys');
-
-var job = new cron.CronJob('* */1 * * * *', function(){
-    sys.puts('You will see this message when the seconds are zero');
-});
